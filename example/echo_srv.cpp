d4e5eb6eb4da9ba78fe9dd561731473686314701
jlijian3 <jlijian3@gmail.com>
2014-06-16 02:13:55 +0000

    change mail

--- example/echo_srv.cpp
+++ example/echo_srv.cpp
@@ -5,11 +5,21 @@
 #include <stdio.h>
 
 #include "dk_core.h"
+#include "ucper.pb.h"
 
-#define RESP_DATA "'HTTP/1.1 200 OK\r\nServer: nginx/0.8.44\r\nContent-length: 5\r\nConnection: keep-alive\r\n\r\nhello"
+#define RESP_DATA_HEADER "HTTP/1.1 200 OK\r\nServer: nginx/0.8.44\r\nContent-length: %d\r\nConnection: keep-alive\r\n\r\n"
 #define dlog1 printf
 
 using namespace std;
+
+#pragma pack(1)
+typedef struct SrvProtoHeader {
+  char      prefix[5];    /* vdc */
+  int32_t   data_len;     /* length of gpb data */
+  char      reserved[7];  /*  reserved */
+  char      data[0];      /*  gpb data */
+} SrvProtoHeader;
+#pragma pack()
 
 class MyServer;
 
@@ -18,6 +28,7 @@ static MyServer *server;
 class SrvConnection: public DKBaseConnection {
 
   virtual void OnConnect() {
+    set_keep_alive(true);
     dlog1("new SrvConnection fd:%d %s:%d\n", fd_, host_.c_str(), port_);
   }
   
@@ -36,29 +47,44 @@ class SrvConnection: public DKBaseConnection {
   }
 
   virtual enum READ_STATUS OnRead() {
-    struct evbuffer *inbuf = get_input_buffer();
-    size_t inbuf_size = evbuffer_get_length(inbuf);
+    SrvProtoHeader *header;
+    int        buf_size;
+    int        data_len;
+    int        total_size;
+    struct evbuffer *buf = get_input_buffer();
+
+    buf_size = evbuffer_get_length(buf);
+  
+    if (buf_size < (int)sizeof(SrvProtoHeader)) {
+      dlog1("[error] ParseFromEvbuffer need at least %d bytes but %d bytes\n",
+            sizeof(SrvProtoHeader), buf_size);
+      return READ_NEED_MORE_DATA;
+    }
+
+    total_size = sizeof(SrvProtoHeader) + data_len;
+    if (buf_size < total_size) {
+      return READ_NEED_MORE_DATA;
+    }
+  
+    header = (SrvProtoHeader *)evbuffer_pullup(buf, sizeof(SrvProtoHeader));
+    data_len = ntohl(header->data_len);
     
-    //dlog1("OnRead %d\n", inbuf_size);
-    if (inbuf_size <= 0)
-      return READ_BAD_CLIENT;
-    
-    char buf[1024];
-    int nread = evbuffer_remove(inbuf, buf, sizeof(buf));
-     
-    if (nread > 0) {
-      
-      if (strncmp(buf, "quit", 4) == 0) {
-        /* close connection after response */
-        set_keep_alive(false);
-      } else {
-        set_keep_alive(true);
-      }
-      Send(buf, nread);
-      //Send(RESP_DATA, strlen(RESP_DATA));
-    } else
-      return READ_BAD_CLIENT;
-    
+    ADD add;
+    add.ParseFromArray(header->data, data_len);
+    add.set_result(add.a() + add.b());
+
+    string reply_buff;
+    add.SerializeToString(&reply_buff);
+
+    SrvProtoHeader reply_header;
+    memset(&reply_header, 0, sizeof(reply_header));
+    reply_header.data_len = htonl(reply_buff.size());
+
+    AddOutputBuffer(&reply_header, sizeof(reply_header));
+    AddOutputBuffer(reply_buff);
+    StartWrite();
+
+    evbuffer_drain(buf, total_size);
     return READ_ALL_DATA;
   }
 };
@@ -93,10 +119,12 @@ int main(int argc, char **argv) {
   
   dlog1("server start ......\n");
 
+  /*
   th_pool.Init(10);
   for (int i=0; i<10000; i++) {
     th_pool.CallInThread(thread_func, (void *)i);
   }
+  */
 
   signal(SIGPIPE, SIG_IGN);
   signal(SIGHUP, SIG_IGN);
@@ -106,12 +134,17 @@ int main(int argc, char **argv) {
     port = atoi(argv[1]);
   }
 
+  int timeout = 600;
+  if (argc > 1) {
+    timeout = atoi(argv[1]);
+  }
+
   if (!server->StartListenTCP(NULL, port, 1024)) {
     dlog1("StartListenTCP fail (:%d)\n", port);
     return 1;
   }
 
-  server->set_timeout(10);
+  server->set_timeout(timeout);
   server->EventLoop();
 
   delete server;
--- example/tcp_client.cpp
+++ example/tcp_client.cpp
@@ -2,75 +2,181 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
+#include <vector>
 
 #include "dk_core.h"
 
-#define dlog1 printf
-#define HOST "localhost"
-#define PORT 60006
+using namespace std;
 
-static DKBaseServer *server;
+#define dlog1 printf
+
+class MyServer;
+class MyConnection;
+static MyServer *server;
+
+static void conn_close_handler(MyConnection *c);
 
 class MyConnection: public DKBaseConnection {
+public:
+  unsigned int last_send_time;
+private:
   virtual enum READ_STATUS OnRead() {
     set_keep_alive(true);
-    char *resp = (char *)evbuffer_pullup(get_input_buffer(), -1);
-    dlog1(">>>>>>>>>>>>>>\n");
-    dlog1("Connection::%s: %s\n", __func__, resp);
-    dlog1("<<<<<<<<<<<<<<\n");
     evbuffer_drain(get_input_buffer(), -1);
-    //server->Stop();
     return READ_ALL_DATA;
   }
 
   virtual void OnConnect() {
-    dlog1("new Connection fd:%d %s:%d\n", fd_, host_.c_str(), port_);
+    //dlog1("new Connection fd:%d %s:%d\n", fd_, host_.c_str(), port_);
   }
   
   virtual void OnClose() {
     dlog1("Connection close fd:%d %s:%d\n",
         fd_, host_.c_str(), port_); 
-    server->Stop();
   }
 
-  virtual void OnError() {
-    dlog1("Connection Error %s\n", this->get_error_string());
-    server->Stop();
+  virtual void OnError(DKConnectionError error) {
+    dlog1("Connection Error %s\n", StrError(error));
   }
 };
 
-static MyConnection *my_conn;
 
-void send_tcp_request(const void *data, int len) {
-  assert(data && len > 0); 
+class MyServer : public DKBaseServer {
+public:
+  MyServer(const char *host, int port,
+           const char *path, int conns,
+           int timeout, int interval, int new_c_per_s) {
+    host_ = host;
+    port_ = port;
+    path_ = path;
+    conns_ = conns;
+    timeout_ = timeout;
+    interval_ = interval;
+    new_c_per_s_ = new_c_per_s;
+    dlog1("host:%s port:%d path:%s conns:%d timeout:%d interval:%d new_c_per_s:%d\n", host, port, path, conns, timeout, interval, new_c_per_s);
+  }
 
-  if (!my_conn) {
-    my_conn = new MyConnection();
-    if (!my_conn) {
+  void collect_closed_conn(MyConnection *c) {
+    //closed_conns_.push(c);
+  }
+
+  void load() {
+    if (new_c_per_s_ > conns_)
+      new_c_per_s_ = conns_;
+    for (int i = 0; i < new_c_per_s_; i++)
+      load_one();
+  }
+
+  void load_one() {
+    if (my_conns_.size() >= conns_)
+      return;
+
+    MyConnection *c = new MyConnection();
+    if (!c) {
       dlog1("new PSConnection failed\n");
       return;
     }
    
-    bool res = my_conn->Init(server->get_base(), -1, HOST, PORT);
+    bool res = c->Init(server->get_base(), -1, host_.c_str(), port_);
     if (!res) {
-      delete my_conn;
-      my_conn = NULL;
-      dlog1("ps_conn_->Init failed\n");
+      delete c;
+      c = NULL;
+      dlog1("conn_->Init failed\n");
       return;
     }
     
-    my_conn->Connect();
-    my_conn->set_timeout(10);
-    my_conn->set_keep_alive(true);
+    c->Connect();
+    c->set_timeout(timeout_);
+    c->set_keep_alive(true);
+    my_conns_.push_back(c);
+    send_data(c);
   }
 
-  my_conn->set_timeout(10);
-  my_conn->set_keep_alive(true);
-  my_conn->Send(data, len);
+  void send_data(MyConnection *c) {
+    char data[1024];
+    int len = sprintf(data,
+        "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: keep-alive\r\n\r\n",
+        path_.c_str(), host_.c_str());
+    c->Send(data, len);
+    c->last_send_time = get_current_time();
+  }
+
+  int check_conns() {
+    int active_conns = 0;
+    int new_conns = 0;
+    for (int i = 0; i < my_conns_.size(); i++) {
+      MyConnection *c = my_conns_[i];
+      if (!c->IsConnected()) {
+        if (new_conns < new_c_per_s_) {
+          send_data(c);
+          new_conns += 1;
+        }
+      }
+      else if (get_current_time() - c->last_send_time > interval_) {
+        send_data(c);
+      }
+
+      if (c->IsConnected()) {
+        active_conns += 1;
+      }
+    }
+
+    return active_conns; 
+  }
+
+  void ClockCallback() {
+    load();
+    int active_conns = check_conns();
+    
+    dlog1("total conns %d, established %d\n", my_conns_.size(), active_conns);
+  }
+
+private:
+  string host_;
+  int port_;
+  int conns_;
+  int timeout_;
+  int interval_;
+  string path_;
+  int new_c_per_s_;
+  int active_conn_;
+  vector<MyConnection *> my_conns_;
+  queue<MyConnection *> closed_conns_;
+};
+
+static void conn_close_handler(MyConnection *c) {
+  if (!server || !c)
+    return;
+
+  server->collect_closed_conn(c);
 }
 
 int main(int argc, char **argv) {
-  server = new DKBaseServer();
+  if (argc < 5) {
+    printf("tcp_client host port path conns [conn_timeout(default 300s)] [secs_per_req(default 10s)]\n");
+    return 1;
+  }
+
+  const char *host = argv[1];
+  int port = atoi(argv[2]);
+  const char *path = argv[3];
+  int conns = atoi(argv[4]);
+
+  int timeout = 300;
+  if (argc >= 6)
+    timeout = atoi(argv[5]);
+
+  int interval = 10;
+  if (argc >= 7)
+    interval = atoi(argv[6]);
+
+  int new_c_per_s = 100; 
+  if (argc >= 8)
+    new_c_per_s = atoi(argv[7]); 
+
+  server = new MyServer(host, port, path, conns,
+      timeout, interval, new_c_per_s);
 
   if (!server || !server->Init()) {
     dlog1("new DKBaseServer Init error\n");
@@ -83,8 +189,7 @@ int main(int argc, char **argv) {
   signal(SIGHUP, SIG_IGN);
 
   dlog1("begin http_load run ....\n");
-  const char *buf = "GET / HTTP/1.1\r\nHost: dev5\r\nConnection: keep-alive\r\n\r\n";
-  send_tcp_request(buf, strlen(buf)); 
+  server->load();
   server->EventLoop();
 
   delete server;
